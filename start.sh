73bb741200d61b22e1957cda98df902ff9131762
JeySerYT <youtubermegase@mail.ru>
2026-06-06 00:36:31 +0300

    fix: rewrite nginx main config to ensure conf.d is included

--- start.sh
+++ start.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 BACKEND_URL=${BACKEND_URL:-http://127.0.0.1:8081}
 export BACKEND_URL
+
 envsubst '${BACKEND_URL}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+
+cat > /etc/nginx/nginx.conf << 'EOF'
+worker_processes auto;
+events {}
+http {
+    include /etc/nginx/mime.types;
+    include /etc/nginx/conf.d/*.conf;
+}
+EOF
+
 exec /usr/bin/supervisord -c /etc/supervisord.conf
